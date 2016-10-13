22ee92ef265ffaacb6a7c3fe77f373d8f3d27c7d
Diallo Hudgins <d@diallohudgins.com>
2016-10-13 15:25:42 -0400

    updated contact page, mobile nav and footer. Added some basic href styles

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -102,7 +102,7 @@
 
 			function updateNavigation() {
 				contentSections.each(function(){
-					$this = $(this);
+					var $this = $(this);
 					var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
 					if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
 						navigationItems.eq(activeSection).addClass('is-selected');
